63251a8a27e29900f82bae72ef87e57bf831bdb7
elya2305 <elinafromua@gmail.com>
2021-01-07 09:59:30 +0200

    gitignore updated

---
+++ src/com/company/array/Target.java
@@ -0,0 +1,36 @@
+package com.company.array;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Target {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(twoSum(new int[]{1, 2, 3, 6, 4}, 7)));
+    }
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> prevMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+
+            if (prevMap.containsKey(diff)) {
+                return new int[]{ prevMap.get(diff), i };
+            }
+            prevMap.put(nums[i], i);
+        }
+        return new int[0]; // Guranteed solution, no need for return
+    }
+
+    public static int[] twoSum2(int[] nums, int target) {
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = i+1; j < nums.length; j++) {
+                if(nums[i] + nums[j] == target) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+        return null;
+    }
+
+}

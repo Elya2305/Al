10221bb5a61f4ac8f4361fc67e31d544fe824a57
Elya <elinafromua@gmail.com>
2021-05-26 08:53:11 +0300

    LinkedList implementation

---
+++ src/com/company/data_structure/linked_list/CustomLinkedList.java
@@ -0,0 +1,120 @@
+package com.company.data_structure.linked_list;
+
+public class CustomLinkedList<T> {
+    private Node<T> head;
+    private int size;
+
+    static class Node<T> {
+        private Node<T> next;
+        private final T value;
+
+        public Node(T value) {
+            this.value = value;
+        }
+    }
+
+    public CustomLinkedList(T value) {
+        this.head = new Node<>(value);
+        this.size = 1;
+    }
+
+    public void addAtTail(T value) {
+        Node<T> newNode = new Node<>(value);
+        Node<T> curr = head;
+        while (curr.next != null) {
+            curr = curr.next;
+        }
+        curr.next = newNode;
+        size++;
+    }
+
+    public void addAtHead(T value) {
+        Node<T> newNode = new Node<>(value);
+        newNode.next = head;
+        this.head = newNode;
+        size++;
+    }
+
+    public void insert(T value, int index) {
+        checkIndexForInsert(index);
+
+        if (index == 0) {
+            addAtHead(value);
+            return;
+        }
+        if (index == size) {
+            addAtTail(value);
+            return;
+        }
+
+        Node<T> newVal = new Node<>(value);
+        Node<T> currVal = head;
+        int start = 0;
+
+        while (start + 1 < index) {
+            currVal = currVal.next;
+            start++;
+        }
+        newVal.next = currVal.next;
+        currVal.next = newVal;
+        size++;
+    }
+
+    public T get(int index) {
+        checkIndex(index);
+        int start = 0;
+        Node<T> curr = head;
+        while (start < index && curr.next != null) {
+            curr = curr.next;
+            start++;
+        }
+        return curr.value;
+    }
+
+    public void deleteAtIndex(int index) {
+        checkIndex(index);
+        int start = 0;
+        Node<T> currVal = head;
+        Node<T> prev = null;
+
+        while (start < index) {
+            prev = currVal;
+            currVal = currVal.next;
+            start++;
+        }
+        if (prev == null) {
+            head = head.next;
+        } else {
+            prev.next = currVal.next;
+        }
+        size--;
+    }
+
+    private void checkIndexForInsert(int index) {
+        if (index > size || index < 0) {
+            throw new IndexOutOfBoundsException("Index " + index + " is out of bounds of length " + size);
+        }
+    }
+
+    private void checkIndex(int index) {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException("Index " + index + " is out of bounds of length " + size);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        Node<T> temp = head;
+        while (temp != null) {
+            if (sb.length() != 1) {
+                sb.append(" ");
+            }
+            sb.append(temp.value).append(",");
+            temp = temp.next;
+        }
+        sb.delete(sb.length() - 1, sb.length());
+        sb.append("]");
+        return sb.toString();
+    }
+}
--- src/com/company/data_structure/linked_list/DesignLinkedList.java
+++ src/com/company/data_structure/linked_list/DesignLinkedList.java
@@ -1,124 +1,13 @@
 package com.company.data_structure.linked_list;
 
-// todo check
 public class DesignLinkedList {
     public static void main(String[] args) {
-        MyLinkedList linkedList = new MyLinkedList(1);
-        linkedList.addAtTail(2);
-        linkedList.addAtTail(3);
-        System.out.println(linkedList);
-        linkedList.deleteAtIndex(1);
+        CustomLinkedList<Integer> linkedList = new CustomLinkedList<>(10);
+        linkedList.addAtTail(11);
+        linkedList.addAtTail(12);
+        linkedList.addAtHead(13);
+        linkedList.deleteAtIndex(4);
+
         System.out.println(linkedList);
     }
 }
-
-class MyLinkedList {
-    private MyLinkedList next;
-    private int val;
-
-    /**
-     * Initialize your data structure here.
-     */
-    public MyLinkedList(int val) {
-        this.val = val;
-    }
-
-    /**
-     * Get the value of the index-th node in the linked list. If the index is invalid, return -1.
-     */
-    public int get(int index) {
-        if (index < 0) return -1;
-        MyLinkedList curr = this;
-        int start = 0;
-        while (start < index && curr != null) {
-            curr = curr.next;
-            start++;
-        }
-        if (start == index && curr != null) {
-            return curr.val;
-        }
-        return -1;
-    }
-
-    /**
-     * Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
-     */
-    public void addAtHead(int val) { //?
-        MyLinkedList newVal = new MyLinkedList(val);
-        MyLinkedList curr = this;
-
-    }
-
-    /**
-     * Append a node of value val to the last element of the linked list.
-     */
-    public void addAtTail(int val) {
-        MyLinkedList newVal = new MyLinkedList(val);
-        MyLinkedList nextVal = this;
-        while (nextVal.next != null) {
-            nextVal = nextVal.next;
-        }
-        nextVal.next = newVal;
-    }
-
-    /**
-     * Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
-     */
-    public void addAtIndex(int index, int val) {
-        if (index == 0) {
-            addAtHead(val);
-        }
-        if (index < 0) return;
-        MyLinkedList newVal = new MyLinkedList(val);
-        MyLinkedList curr = this;
-        int start = 1;
-        while (start < index && curr != null) {
-            curr = curr.next;
-            start++;
-        }
-        if (start != index || curr == null) return;
-        MyLinkedList temp = curr.next;
-        curr.next = newVal;
-        newVal.next = temp;
-    }
-
-    /**
-     * Delete the index-th node in the linked list, if the index is valid.
-     */
-    public void deleteAtIndex(int index) {
-        if (index < 0) return;
-        int start = 0;
-        MyLinkedList curr = this;
-        MyLinkedList previous = null;
-        while (start < index && curr != null) {
-            curr = curr.next;
-            previous = curr;
-            start++;
-        }
-         if (curr == null || previous == null) return;
-
-        previous.next = curr.next;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder res = new StringBuilder("[" + val);
-        MyLinkedList next = this.next;
-        while (next != null) {
-            res.append(", ").append(next.val);
-            next = next.next;
-        }
-        res.append("]");
-        return res.toString();
-    }
-}
-
-/**
- * Your MyLinkedList object will be instantiated and called as such:
- * MyLinkedList obj = new MyLinkedList();
- * int param_1 = obj.get(index);
- * obj.addAtHead(val);
- * obj.addAtTail(val);
- * obj.addAtIndex(index,val);
- * obj.deleteAtIndex(index);
- */
